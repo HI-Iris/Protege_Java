8f6cd672fadd2310a5d2305c921a2e011371e164
iris.hou <xinzhu.hou@gmail,com>
2019-02-20 10:30:23 +1100

    delete .idea

--- magic_year/src/ValidationTest.java
+++ magic_year/src/ValidationTest.java
@@ -2,9 +2,9 @@ import junit.framework.TestCase;
 
 public class UtilityTest extends TestCase {
     public void testIsNum(){
-        String str1 = "sdfsa";
-        String str2 = "12321";
-        String str3 = "123fdasdfs";
+        String str1 = "this is not a number";
+        String str2 = "123";
+        String str3 = "123 and letters combination";
         Utility testUtility = new Utility();
         boolean str1Test = testUtility.isNum(str1);
         boolean str2Test = testUtility.isNum(str2);
