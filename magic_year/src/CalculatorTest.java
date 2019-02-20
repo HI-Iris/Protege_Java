2cc598fd0c64481890ebe91270e4e0da531bb311
iris.hou <xinzhu.hou@gmail,com>
2019-02-20 11:15:57 +1100

    Improve code readability

--- magic_year/src/CalculatorTest.java
+++ magic_year/src/CalculatorTest.java
@@ -1,10 +1,39 @@
 import junit.framework.TestCase;
 
 public class CalculatorTest extends TestCase {
+    Calculator testCalculator = new Calculator();
 
     public void testMagicYear() {
+        int yearStartWork = 0;
+        assertEquals(65, testCalculator.magicYear(yearStartWork));
     }
 
-    public void testMonthSalary() {
+    public void test_round_produce_$1_when_the_month_salary_is_50c() {
+        int annualSalary = 6;
+        assertEquals(1,testCalculator.monthSalary(annualSalary));
     }
+
+    public void test_round_produce_$0_when_the_month_salary_is_25c() {
+        int annualSalary = 3;
+        assertEquals(0,testCalculator.monthSalary(annualSalary));
+    }
+
+//    //what would "round" produce when the month salary is *.49
+//    public void testMonthSalary() {
+//
+//    }
+
+    public void test_round_produce_$0_when_the_month_salary_is_neg25c() {
+        int annualSalary = -3;
+        assertEquals(0,testCalculator.monthSalary(annualSalary));
+    }
+
+//    //what would "round" produce when the month salary is -*
+//    public void testNegativeMonthSalary() {
+//        int neg
+//    }
+//    //what would "round" produce when the month salary is -*.49
+//    public void testMonthSalary() {
+//
+//    }
 }
