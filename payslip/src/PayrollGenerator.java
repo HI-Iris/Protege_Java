366a2e2bd12f342096b65d36937bc2d7ce38e4f6
iris.hou <xinzhu.hou@gmail,com>
2019-02-21 10:27:32 +1100

    Change functions names, make package private, remove redundant if statement

--- payslip/src/PayrollGenerator.java
+++ payslip/src/PayrollGenerator.java
@@ -1,24 +1,24 @@
-public class PayrollGenerator {
-    public static final int NUM_OF_MONTH = 12;
+class PayrollGenerator {
+    private static final int NUM_OF_MONTH = 12;
     private double grossTax;
     private int incomeTax;
 
-    public int grossIncome(int salary) {
+    int getGrossIncome(int salary) {
         return Math.round(salary / NUM_OF_MONTH);
     }
 
-    public int superAmount(int grossIncome, int superRate) {
+    int getSuperAmount(int grossIncome, int superRate) {
         return Math.round(grossIncome * superRate / 100);
     }
 
-    public int incomeTax(int annualSalary) {
+    int getIncomeTax(int annualSalary) {
         if (annualSalary <= 18200) {
             grossTax = 0;
-        } else if (18200 < annualSalary && annualSalary <= 37000) {
+        } else if (annualSalary <= 37000) {
             grossTax = Math.floor((annualSalary - 18200) * 0.19);
-        } else if (37000 < annualSalary && annualSalary <= 87000) {
+        } else if (annualSalary <= 87000) {
             grossTax = 3572 + Math.floor((annualSalary - 37000) * 0.325);
-        } else if (87000 < annualSalary && annualSalary <= 180000) {
+        } else if (annualSalary <= 180000) {
             grossTax = 19822 + Math.floor((annualSalary - 87000) * 0.37);
         } else {
             grossTax = 54232 + Math.floor((annualSalary - 180000) * 0.45);
