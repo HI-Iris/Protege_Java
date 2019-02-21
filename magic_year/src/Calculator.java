9e5c788a17182c54a11074f160d9b20453e4d9d5
iris.hou <xinzhu.hou@gmail,com>
2019-02-21 17:14:29 +1100

    Rename functions

--- magic_year/src/Calculator.java
+++ magic_year/src/Calculator.java
@@ -1,12 +1,12 @@
-public class Calculator {
+class Calculator {
     private static final float NUM_OF_MONTH = 12;
     private static final int MAGIC_YEAR = 65;
 
-    int magicYear(int yearStartWork) {
+    int getMagicYearByYearStartWork(int yearStartWork) {
         return yearStartWork + MAGIC_YEAR;
     }
 
-    int monthSalary(int annualSalary) {
+    int getMonthSalaryByAnnualSalary(int annualSalary) {
         return Math.round(annualSalary / NUM_OF_MONTH);
     }
 }
