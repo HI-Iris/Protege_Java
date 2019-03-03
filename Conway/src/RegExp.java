b3d3996bfdc2a6dc6fa7c6b0d6ad391cae7da404
HI-Iris <xinzhu.hou@gmail.com>
2019-03-04 10:25:12 +1100

    Refine validation class, pass regular expression as parameter

---
+++ Conway/src/RegExp.java
@@ -0,0 +1,5 @@
+class RegExp {
+    static final String VALID_BOARD_SIZE = "^[0-9]+\\*[0-9]+$";
+    static final String VALID_COORDS = "^[0-9]+\\s[0-9]+$";
+    static final String VALID_QUIT_CMD = "^[Qq]$";
+}
--- Conway/src/ValidationTest.java
+++ Conway/src/ValidationTest.java
@@ -3,24 +3,27 @@ import junit.framework.TestCase;
 public class ValidationTest extends TestCase {
     public void testValidBoardSize() {
         String validBoardSize ="6*8";
-        assertTrue(Validation.isValidBoardSize(validBoardSize));
+        assertTrue(Validation.isValid(validBoardSize, RegExp.VALID_BOARD_SIZE));
     }
-    public void testVInvalidBoardSize() {
+    public void testInvalidBoardSize() {
         String invalidBoardSize ="*";
-        assertFalse(Validation.isValidBoardSize(invalidBoardSize));
+        assertFalse(Validation.isValid(invalidBoardSize, RegExp.VALID_BOARD_SIZE));
     }
-
-    public void testValidCommandCoord() {
-        String validCommandCoord ="6 8";
-        assertTrue(Validation.isValidCoord(validCommandCoord));
+    public void testValidCoords() {
+        String validCoords ="6 8";
+        assertTrue(Validation.isValid(validCoords, RegExp.VALID_COORDS));
+    }
+    public void testInvalidCoords() {
+        String  invalidCoords= "this is invalid";
+        assertFalse(Validation.isValid(invalidCoords, RegExp.VALID_COORDS));
     }
     public void testValidCommandQuit() {
-        String validCommandQuit ="q";
-        assertTrue(Validation.isValidCoord(validCommandQuit));
+        String validQuitCmd ="q";
+        assertTrue(Validation.isValid(validQuitCmd, RegExp.VALID_QUIT_CMD));
     }
     public void testInvalidCommand() {
-        String  invalidCommand= "this is invalid";
-        assertFalse(Validation.isValidCoord(invalidCommand));
+        String  invalidQuit= "this is invalid";
+        assertFalse(Validation.isValid(invalidQuit, RegExp.VALID_QUIT_CMD));
     }
 
 
