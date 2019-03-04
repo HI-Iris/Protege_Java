7d3827d1b085b03804bc5a28adcb062f18344083
HI-Iris <xinzhu.hou@gmail.com>
2019-03-04 17:45:05 +1100

    Set initial world by user input, and calculate the first generation

--- Conway/src/Main.java
+++ Conway/src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-//        String validSize = IDK.getValidBoardSizeByUserInput();
-//        Board emptyBoard = IDK.buildBoardByValidSize(validSize);
-//        IDK.printCurrentBoard(emptyBoard);
-//        Board firstGenBoard = IDK.initialBoardByUserInput(emptyBoard);
-//        IDK.printCurrentBoard(firstGenBoard);
-
-        Cell[][] cells = new Cell[][]{{Cell.live, Cell.live, Cell.dead}, {Cell.live, Cell.live, Cell.dead}, {Cell.live, Cell.live, Cell.dead}};
-        Board currentBoard = new Board(cells);
-        IDK.printCurrentBoard(currentBoard);
-        Board nextGen = IDK.nextGen(currentBoard);
+        String validSize = IDK.getValidBoardSizeByUserInput();
+        Board emptyBoard = IDK.buildBoardByValidSize(validSize);
+        IDK.printCurrentBoard(emptyBoard);
+        Board firstGenBoard = IDK.initialBoardByUserInput(emptyBoard);
+        IDK.printCurrentBoard(firstGenBoard);
+        Board nextGen = IDK.nextGen(firstGenBoard);
         IDK.printCurrentBoard(nextGen);
-
     }
 }
