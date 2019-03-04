f33a1cc9cc9475efc23c85fc72f81bb255a37cf5
HI-Iris <xinzhu.hou@gmail.com>
2019-03-04 17:41:06 +1100

    Hard code the initial world (test the performance)

--- Conway/src/Main.java
+++ Conway/src/Main.java
@@ -1,11 +1,16 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        String validSize = IDK.getValidBoardSizeByUserInput();
-        Board myBoard = IDK.buildBoardByValidSize(validSize);
-        IDK.printCurrentBoard(myBoard);
-        Board firstGenBoard = IDK.initialBoardByUserInput(myBoard);
-        IDK.printCurrentBoard(firstGenBoard);
+public class Main {
+    public static void main(String[] args) {
+//        String validSize = IDK.getValidBoardSizeByUserInput();
+//        Board emptyBoard = IDK.buildBoardByValidSize(validSize);
+//        IDK.printCurrentBoard(emptyBoard);
+//        Board firstGenBoard = IDK.initialBoardByUserInput(emptyBoard);
+//        IDK.printCurrentBoard(firstGenBoard);
+
+        Cell[][] cells = new Cell[][]{{Cell.live, Cell.live, Cell.dead}, {Cell.live, Cell.live, Cell.dead}, {Cell.live, Cell.live, Cell.dead}};
+        Board currentBoard = new Board(cells);
+        IDK.printCurrentBoard(currentBoard);
+        Board nextGen = IDK.nextGen(currentBoard);
+        IDK.printCurrentBoard(nextGen);
+
     }
 }
