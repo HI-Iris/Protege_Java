e500c8b803515b42bc2c00462dad5fed11f602ab
HI-Iris <xinzhu.hou@gmail.com>
2019-03-05 15:06:56 +1100

    Refine print function

--- Conway/src/SysPrint.java
+++ Conway/src/SysPrint.java
@@ -1,35 +1,11 @@
 class SysPrint {
-    void printWelcomeMsg() {
-        System.out.println("\n*-*-*-*-*- Welcome to Conway's game of life! -*-*-*-*-*\n");
-    }
-
-    void printSizeEnter() {
-        System.out.println("Please set your board size x*y (e.g. 9*9)");
-    }
-
-    void printBoardSize() {
-        System.out.println("\n*-*-*-*-*- This is the size of your board -*-*-*-*-*\n");
-    }
-
-    void printSizeInvalid() {
-        System.out.println("Sorry, please enter a valid board size x*y (e.g. 9*9)");
+    void printMsg(String msg) {
+        System.out.println(msg);
     }
 
     void printSeparator(int generation, int numOfLifeCellOnBoard) {
         System.out.println("\n*-*-*-*-*- Generation: " + generation + " -*-*-*-*-*");
         System.out.println("Live cells on board: " + numOfLifeCellOnBoard + "\n");
-    }
-
-    void printCoordsEnter() {
-        System.out.println("\nPlease enter the coords (x y) of the live ceil to set the initial world (e.g. 0 0), and enter 'Q' to finish: ");
-    }
-
-    void printCoordsOutOfRange() {
-        System.out.println("Sorry, the coordinate you have entered is out of range.");
-    }
-
-    void printCoordsInvalid() {
-        System.out.println("Sorry, please enter a valid coordinate x y (e.g. 5 6) ");
     }
 
     void printCurrentBoard(Board currentBoard) {
