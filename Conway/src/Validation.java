1cd523bb494eb4891df123b43bdfadad57a4b7da
HI-Iris <xinzhu.hou@gmail.com>
2019-03-05 13:45:57 +1100

    Remove console print,

--- Conway/src/Validation.java
+++ Conway/src/Validation.java
@@ -1,37 +1,12 @@
 class Validation {
-
     static boolean isValid(String userInput, String regExp) {
         return userInput.matches(regExp);
-    }
-
-    static boolean isValidBoardSize(String userInput) {
-        String boardRegex = "^[0-9]+\\*[0-9]+$";
-        if (userInput.matches(boardRegex)) return true;
-        else {
-            System.out.println("Sorry, please enter a valid board size x*y (e.g. 9*9)");
-            return false;
-        }
-    }
-
-    static boolean isValidCoord(String userInput) {
-        String boardRegex = "^[0-9]+\\s[0-9]+$";
-        if (userInput.matches(boardRegex)) return true;
-        else {
-            System.out.println("Sorry, please enter a valid coordinate x y (e.g. 5 6) ");
-            return false;
-        }
     }
 
     static boolean isCoordsInRange(String userInput, int numOfRows, int numOfColumns) {
         String[] coords = userInput.split(" ");
         int rowFromInput = Integer.parseInt(coords[0]);
-        int colunmFromInput = Integer.parseInt(coords[1]);
-        if (rowFromInput < numOfRows && colunmFromInput < numOfColumns) return true;
-        else {
-            System.out.println("Sorry, the coordinate you have entered is out of range.");
-            return false;
-        }
-
-
+        int columnFromInput = Integer.parseInt(coords[1]);
+        return rowFromInput < numOfRows && columnFromInput < numOfColumns;
     }
 }
