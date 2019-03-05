c14533c1d9b8de9354ce9252c8e16a76f0d910bf
iris.hou <xinzhu.hou@gmail,com>
2019-03-05 15:49:33 +1100

    Add each new generation to a board list, print the last generation, and pass the last generation to next round

--- Conway/src/Main.java
+++ Conway/src/Main.java
@@ -1,11 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        SysPrint myPrinter = new SysPrint();
+        List<Board> boardList = new ArrayList<>();
+        myPrinter.printMsg(ConstString.MSG_WELCOME);
+        myPrinter.printMsg(ConstString.MSG_ENTER_SIZE);
         String validSize = IDK.getValidBoardSizeByUserInput();
         Board emptyBoard = IDK.buildBoardByValidSize(validSize);
-        IDK.printCurrentBoard(emptyBoard);
-        Board firstGen = IDK.initialBoardByUserInput(emptyBoard);
-        IDK.printCurrentBoard(firstGen);
-        Board nextGen = IDK.nextGen(firstGen);
-        IDK.printCurrentBoard(nextGen);
+        myPrinter.printMsg(ConstString.MSG_SIZE);
+        myPrinter.printCurrentBoard(emptyBoard);
+        myPrinter.printMsg(ConstString.MSG_ENTER_COORDS);
+        Board tempBoard = IDK.initialBoardByUserInput(emptyBoard);
+        boardList.add(tempBoard);
+        int numOfLiveCellOnBoard;
+        do {
+            int generation = boardList.size();
+            tempBoard = boardList.get(generation - 1);
+            numOfLiveCellOnBoard = IDK.getNumOfLiveCellOnCurrentBoard(tempBoard);
+            myPrinter.printSeparator(generation, numOfLiveCellOnBoard);
+            myPrinter.printCurrentBoard(tempBoard);
+            boardList.add(IDK.nextGen(tempBoard));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (numOfLiveCellOnBoard != 0);
+
+
     }
 }
