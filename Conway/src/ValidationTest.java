b325a810398e11cd9d4dedde84f3e20a4b09a58b
HI-Iris <xinzhu.hou@gmail.com>
2019-03-05 15:14:46 +1100

    Rename class

--- Conway/src/ValidationTest.java
+++ Conway/src/ValidationTest.java
@@ -3,27 +3,27 @@ import junit.framework.TestCase;
 public class ValidationTest extends TestCase {
     public void testValidBoardSize() {
         String validBoardSize ="6*8";
-        assertTrue(Validation.isValid(validBoardSize, RegExp.VALID_BOARD_SIZE));
+        assertTrue(Validation.isValid(validBoardSize, ConstString.VALID_BOARD_SIZE));
     }
     public void testInvalidBoardSize() {
         String invalidBoardSize ="*";
-        assertFalse(Validation.isValid(invalidBoardSize, RegExp.VALID_BOARD_SIZE));
+        assertFalse(Validation.isValid(invalidBoardSize, ConstString.VALID_BOARD_SIZE));
     }
     public void testValidCoords() {
         String validCoords ="6 8";
-        assertTrue(Validation.isValid(validCoords, RegExp.VALID_COORDS));
+        assertTrue(Validation.isValid(validCoords, ConstString.VALID_COORDS));
     }
     public void testInvalidCoords() {
         String  invalidCoords= "this is invalid";
-        assertFalse(Validation.isValid(invalidCoords, RegExp.VALID_COORDS));
+        assertFalse(Validation.isValid(invalidCoords, ConstString.VALID_COORDS));
     }
     public void testValidCommandQuit() {
         String validQuitCmd ="q";
-        assertTrue(Validation.isValid(validQuitCmd, RegExp.VALID_QUIT_CMD));
+        assertTrue(Validation.isValid(validQuitCmd, ConstString.VALID_QUIT_CMD));
     }
     public void testInvalidCommand() {
         String  invalidQuit= "this is invalid";
-        assertFalse(Validation.isValid(invalidQuit, RegExp.VALID_QUIT_CMD));
+        assertFalse(Validation.isValid(invalidQuit, ConstString.VALID_QUIT_CMD));
     }
 
 
