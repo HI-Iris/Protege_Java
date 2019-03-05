a7f9dc818265b4a07218a0ce01f15c63418c6eab
HI-Iris <xinzhu.hou@gmail.com>
2019-03-05 15:07:55 +1100

    Refine print function, move String into a class

--- Conway/src/RegExp.java
+++
@@ -1,5 +0,0 @@
-class RegExp {
-    static final String VALID_BOARD_SIZE = "^[0-9]+\\*[0-9]+$";
-    static final String VALID_COORDS = "^[0-9]+\\s[0-9]+$";
-    static final String VALID_QUIT_CMD = "^[Qq]$";
-}
