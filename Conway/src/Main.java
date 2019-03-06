56c996ab2dca0ce9da60aeaa72563f8f287249a4
iris.hou <xinzhu.hou@gmail,com>
2019-03-07 08:57:54 +1100

    Rename the class

--- Conway/src/Main.java
+++ Conway/src/Main.java
@@ -6,21 +6,21 @@ public class Main {
         List<Board> boardList = new ArrayList<>();
         SysPrint.printMsg(ConstString.MSG_WELCOME);
         SysPrint.printMsg(ConstString.MSG_ENTER_SIZE);
-        String validSize = IDK.getValidBoardSizeByUserInput();
-        Board emptyBoard = IDK.buildBoardByValidSize(validSize);
+        String validSize = BoardService.getValidBoardSizeByUserInput();
+        Board emptyBoard = BoardService.buildBoardByValidSize(validSize);
         SysPrint.printMsg(ConstString.MSG_SIZE);
         SysPrint.printCurrentBoard(emptyBoard);
         SysPrint.printMsg(ConstString.MSG_ENTER_COORDS);
-        Board tempBoard = IDK.initialBoardByUserInput(emptyBoard);
+        Board tempBoard = BoardService.initialBoardByUserInput(emptyBoard);
         boardList.add(tempBoard);
         int numOfLiveCellOnBoard;
         do {
             int generation = boardList.size();
             tempBoard = boardList.get(generation - 1);
-            numOfLiveCellOnBoard = IDK.getNumOfLiveCellOnCurrentBoard(tempBoard);
+            numOfLiveCellOnBoard = BoardService.getNumOfLiveCellOnCurrentBoard(tempBoard);
             SysPrint.printSeparator(generation, numOfLiveCellOnBoard);
             SysPrint.printCurrentBoard(tempBoard);
-            boardList.add(IDK.nextGen(tempBoard));
+            boardList.add(BoardService.nextGen(tempBoard));
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
