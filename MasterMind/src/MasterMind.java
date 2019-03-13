1894a9042d62b0a44321c49874fd999d5e0f5038
iris.hou <xinzhu.hou@gmail,com>
2019-03-13 11:00:23 +1100

    Color human and masterMind chanegs

--- MasterMind/src/MasterMind.java
+++ MasterMind/src/MasterMind.java
@@ -1,3 +1,25 @@
-class MasterMind implements Players{
+import java.util.Random;
+
+class MasterMind {
+    Color[] masterColor = new Color[ConstString.NUM_OF_COLOR];
+
+    private int seedNum = (int) (Math.random() * 6);
+    static int randomNum(){
+        return (int) (Math.random() * 6);
+    }
+    private Random randomObj = new Random();
+    private <T extends Enum<T>> T getRandomColor(Class<T> color) {
+        return random(color.getEnumConstants());
+    }
+    private <T> T random(T[] values) {
+        return values[randomObj.nextInt(values.length)];
+    }
+
+    MasterMind() {
+        for (int i = 0; i < ConstString.NUM_OF_COLOR; i++) {
+            this.masterColor[i] = getRandomColor(Color.class);
+            
+        }
+    }
 
 }
