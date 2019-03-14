89d45a881876c03c692a22b7b1ffda7ebf0261fc
iris.hou <xinzhu.hou@gmail,com>
2019-03-14 13:51:40 +1100

    Check whether the String is a Color

--- MasterMind/src/Validation.java
+++ MasterMind/src/Validation.java
@@ -1,2 +1,9 @@
 class Validation {
+    static boolean isColor(String userInput) {
+        for (Color color : Color.values())
+            if (userInput.toUpperCase().equals(color.name().toUpperCase())) {
+                return true;
+            }
+        return false;
+    }
 }
