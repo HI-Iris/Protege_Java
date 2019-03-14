a4e5eceda5d0f95de2b80c279d307e5e996494ba
iris.hou <xinzhu.hou@gmail,com>
2019-03-14 13:38:46 +1100

    Dependency injection, use constructor to initialize instance

--- MasterMind/src/MasterMind.java
+++ MasterMind/src/MasterMind.java
@@ -1,25 +1,11 @@
-import java.util.Random;
-
 class MasterMind {
-    Color[] masterColor = new Color[ConstString.NUM_OF_COLOR];
+    private Color[] masterColors;
 
-    private int seedNum = (int) (Math.random() * 6);
-    static int randomNum(){
-        return (int) (Math.random() * 6);
-    }
-    private Random randomObj = new Random();
-    private <T extends Enum<T>> T getRandomColor(Class<T> color) {
-        return random(color.getEnumConstants());
-    }
-    private <T> T random(T[] values) {
-        return values[randomObj.nextInt(values.length)];
+    MasterMind (Color[] masterColors){
+        this.masterColors = masterColors;
     }
 
-    MasterMind() {
-        for (int i = 0; i < ConstString.NUM_OF_COLOR; i++) {
-            this.masterColor[i] = getRandomColor(Color.class);
-            
-        }
+    Color[] getMasterColors() {
+        return masterColors;
     }
-
 }
