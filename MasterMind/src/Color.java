d57eb5c661d42284ac0956dbeb0664d1481f2e34
iris.hou <xinzhu.hou@gmail,com>
2019-03-14 13:36:36 +1100

    Remove index

--- MasterMind/src/Color.java
+++ MasterMind/src/Color.java
@@ -1,16 +1,8 @@
 enum Color {
-    Red(0),
-    Blue(1),
-    Green(2),
-    Orange(3),
-    Purple(4),
-    Yellow(5);
-
-    private final int index;
-
-    Color(int index) {
-        this.index = index;
-
-    }
-
+    Red,
+    Blue,
+    Green,
+    Orange,
+    Purple,
+    Yellow
 }
