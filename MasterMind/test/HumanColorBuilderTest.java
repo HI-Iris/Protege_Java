420679c103973a9dbd6c08cf8990a9583bbaf9ec
iris.hou <xinzhu.hou@gmail,com>
2019-03-18 10:08:04 +1100

    Replace array with list

---
+++ MasterMind/test/HumanColorBuilderTest.java
@@ -0,0 +1,63 @@
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class HumanColorBuilderTest {
+//    private HumanColorBuilder humanColorBuilder = new HumanColorBuilder();
+//
+//    @Test
+//    public void givenStringRedReturnColorArrayRed() {
+//        String[] userInput = {"Red"};
+//        Color[] expectedColor = {Color.Red};
+//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
+//        assertArrayEquals(expectedColor, resultColor);
+//    }
+//
+//    @Test
+//    public void givenStringBlueReturnColorArrayBlue() {
+//        String[] userInput = {"Blue"};
+//        Color[] expectedColor = {Color.Blue};
+//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
+//        assertArrayEquals(expectedColor, resultColor);
+//    }
+//
+//    @Test
+//    public void givenStringYellowReturnColorArrayYellow() {
+//        String[] userInput = {"Yellow"};
+//        Color[] expectedColor = {Color.Yellow};
+//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
+//        assertArrayEquals(expectedColor, resultColor);
+//    }
+//
+//    @Test
+//    public void givenStringGreenReturnColorArrayGreen() {
+//        String[] userInput = {"Green"};
+//        Color[] expectedColor = {Color.Green};
+//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
+//        assertArrayEquals(expectedColor, resultColor);
+//    }
+//
+//    @Test
+//    public void givenStringPurpleReturnColorArrayPurple() {
+//        String[] userInput = {"Purple"};
+//        Color[] expectedColor = {Color.Purple};
+//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
+//        assertArrayEquals(expectedColor, resultColor);
+//    }
+//
+//    @Test
+//    public void givenStringOrangeReturnColorArrayOrange() {
+//        String[] userInput = {"Orange"};
+//        Color[] expectedColor = {Color.Orange};
+//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
+//        assertArrayEquals(expectedColor, resultColor);
+//    }
+//
+//    @Test
+//    public void givenStringArrayFourPurpleReturnColorArrayFourPurple() {
+//        String[] userInput = {"Purple", "Purple", "Purple", "Purple"};
+//        Color[] expectedColor = {Color.Purple, Color.Purple, Color.Purple, Color.Purple};
+//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
+//        assertArrayEquals(expectedColor, resultColor);
+//    }
+//}
