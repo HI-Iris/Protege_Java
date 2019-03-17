81baf6f15c7921690ce2ab2f3d6dbd55b35d9528
HI-Iris <xinzhu.hou@gmail.com>
2019-03-18 10:07:51 +1100

    Replace array with list

--- MasterMind/src/GameRules.java
+++ MasterMind/src/GameRules.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 class GameRules {
-    static Result[] match(Color[] humanColor, MasterMind masterMind) {
-        ArrayList<Result> result = new ArrayList<>();
-        for (int i = 0; i < humanColor.length; i++) {
-            if (humanColor[i] == masterMind.getMasterColors()[i]) {
+    static List<Result> match(List<Color> humanColor, MasterMind masterMind) {
+        List<Color> masterColors = masterMind.getMasterColors();
+        List<Result> result = new ArrayList<>();
+        for (int i = 0; i < humanColor.size(); i++) {
+            if (humanColor.get(i) == masterColors.get(i)) {
                 result.add(Result.Black);
             }
-            if ((Arrays.asList(masterMind.getMasterColors()).contains(humanColor[i])) && (humanColor[i] != masterMind.getMasterColors()[i])){
+            if ((masterColors.contains(humanColor.get(i))) && (humanColor.get(i) != masterColors.get(i))) {
                 result.add(Result.White);
             }
         }
-        return result.toArray(Result[]::new);
+        return result;
     }
 }
 
