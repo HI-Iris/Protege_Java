da215c31568b6e65f0724fd62765296fb845d5b2
iris.hou <xinzhu.hou@gmail,com>
2019-03-18 10:07:13 +1100

    Replace array with list

--- MasterMind/src/HumanColorBuilder.java
+++ MasterMind/src/HumanColorBuilder.java
@@ -1,6 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class HumanColorBuilder implements ColorBuilder {
     @Override
-    public Color[] buildColor() {
-        return new Color[0];
+    public List<Color> buildColor(String[]... userInput) {
+        int num = userInput[0].length;
+        List<Color> result= new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            switch (userInput[0][i]) {
+                case "Red":
+                    result.add(Color.Red);
+                    break;
+                case "Blue":
+                    result.add(Color.Blue);
+                    break;
+                case "Yellow":
+                    result.add(Color.Yellow);
+                    break;
+                case "Green":
+                    result.add(Color.Green);
+                    break;
+                case "Purple":
+                    result.add(Color.Purple);
+                    break;
+                case "Orange":
+                    result.add(Color.Orange);
+                    break;
+                default:
+                    break;
+            }
+        }
+        return result;
     }
 }
