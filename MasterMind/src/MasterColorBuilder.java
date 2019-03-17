92248ec262127e357d27c84d128977891cfa67fd
HI-Iris <xinzhu.hou@gmail.com>
2019-03-18 10:07:13 +1100

    Replace array with list

--- MasterMind/src/MasterColorBuilder.java
+++ MasterMind/src/MasterColorBuilder.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class MasterColorBuilder implements ColorBuilder{
     private static int getRandomColorIndex() {
-        return (int) (Math.random() * ConstString.NUM_OF_COLOR_IN_TOTAL);
+        return (int) (Math.random() * Constants.NUM_OF_COLOR_IN_TOTAL);
     }
 
     @Override
-    public Color[] buildColor() {
-        Color[] masterColor = new Color[4];
-        for (int i = 0; i < ConstString.NUM_OF_COLOR_IN_GAME; i++) {
-            masterColor[i] = Color.values()[getRandomColorIndex()];
+    public List<Color> buildColor(String[]... userInput) {
+        List<Color> masterColor = new ArrayList<>();
+        for (int i = 0; i < Constants.NUM_OF_COLOR_IN_GAME; i++) {
+            masterColor.add(Color.values()[getRandomColorIndex()]);
         }
         return masterColor;
     }
