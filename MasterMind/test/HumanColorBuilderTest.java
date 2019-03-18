bda16ca87d3aff75863032b62bacac77f29e8416
iris.hou <xinzhu.hou@gmail.com>
2019-03-18 14:49:10 +1100

    Refactor the rest tests, replace array with list

--- MasterMind/test/HumanColorBuilderTest.java
+++ MasterMind/test/HumanColorBuilderTest.java
@@ -12,7 +12,7 @@ public class HumanColorBuilderTest {
     private List<Color> resultColor;
 
     @Test
-    public void givenStringRedReturnColorArrayRed() {
+    public void givenListStringRedReturnListColorRed() {
         this.humanColorBuilder = new HumanColorBuilder();
         this.expectedColor = new ArrayList<>();
         this.resultColor = new ArrayList<>();
@@ -20,52 +20,67 @@ public class HumanColorBuilderTest {
         expectedColor.add(Color.Red);
         assertThat(resultColor, equalTo(expectedColor));
     }
-//
-//    @Test
-//    public void givenStringBlueReturnColorArrayBlue() {
-//        String[] userInput = {"Blue"};
-//        Color[] expectedColor = {Color.Blue};
-//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
-//        assertArrayEquals(expectedColor, resultColor);
-//    }
-//
-//    @Test
-//    public void givenStringYellowReturnColorArrayYellow() {
-//        String[] userInput = {"Yellow"};
-//        Color[] expectedColor = {Color.Yellow};
-//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
-//        assertArrayEquals(expectedColor, resultColor);
-//    }
-//
-//    @Test
-//    public void givenStringGreenReturnColorArrayGreen() {
-//        String[] userInput = {"Green"};
-//        Color[] expectedColor = {Color.Green};
-//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
-//        assertArrayEquals(expectedColor, resultColor);
-//    }
-//
-//    @Test
-//    public void givenStringPurpleReturnColorArrayPurple() {
-//        String[] userInput = {"Purple"};
-//        Color[] expectedColor = {Color.Purple};
-//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
-//        assertArrayEquals(expectedColor, resultColor);
-//    }
-//
-//    @Test
-//    public void givenStringOrangeReturnColorArrayOrange() {
-//        String[] userInput = {"Orange"};
-//        Color[] expectedColor = {Color.Orange};
-//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
-//        assertArrayEquals(expectedColor, resultColor);
-//    }
-//
-//    @Test
-//    public void givenStringArrayFourPurpleReturnColorArrayFourPurple() {
-//        String[] userInput = {"Purple", "Purple", "Purple", "Purple"};
-//        Color[] expectedColor = {Color.Purple, Color.Purple, Color.Purple, Color.Purple};
-//        Color[] resultColor = humanColorBuilder.buildColor(userInput);
-//        assertArrayEquals(expectedColor, resultColor);
-//    }
+
+    @Test
+    public void givenListStringBlueReturnListColorBlue() {
+        this.humanColorBuilder = new HumanColorBuilder();
+        this.expectedColor = new ArrayList<>();
+        this.resultColor = new ArrayList<>();
+        resultColor = humanColorBuilder.buildColor(new String[]{"Blue"});
+        expectedColor.add(Color.Blue);
+        assertThat(resultColor, equalTo(expectedColor));
+    }
+
+    @Test
+    public void givenListStringYellowReturnListColorYellow() {
+        this.humanColorBuilder = new HumanColorBuilder();
+        this.expectedColor = new ArrayList<>();
+        this.resultColor = new ArrayList<>();
+        resultColor = humanColorBuilder.buildColor(new String[]{"Yellow"});
+        expectedColor.add(Color.Yellow);
+        assertThat(resultColor, equalTo(expectedColor));
+    }
+
+    @Test
+    public void givenListStringGreenReturnListColorGreen() {
+        this.humanColorBuilder = new HumanColorBuilder();
+        this.expectedColor = new ArrayList<>();
+        this.resultColor = new ArrayList<>();
+        resultColor = humanColorBuilder.buildColor(new String[]{"Green"});
+        expectedColor.add(Color.Green);
+        assertThat(resultColor, equalTo(expectedColor));
+    }
+
+    @Test
+    public void givenListStringPurpleReturnListColorPurple() {
+        this.humanColorBuilder = new HumanColorBuilder();
+        this.expectedColor = new ArrayList<>();
+        this.resultColor = new ArrayList<>();
+        resultColor = humanColorBuilder.buildColor(new String[]{"Purple"});
+        expectedColor.add(Color.Purple);
+        assertThat(resultColor, equalTo(expectedColor));
+    }
+
+    @Test
+    public void givenListStringOrangeReturnListColorOrange() {
+        this.humanColorBuilder = new HumanColorBuilder();
+        this.expectedColor = new ArrayList<>();
+        this.resultColor = new ArrayList<>();
+        resultColor = humanColorBuilder.buildColor(new String[]{"Orange"});
+        expectedColor.add(Color.Orange);
+        assertThat(resultColor, equalTo(expectedColor));
+    }
+
+    @Test
+    public void givenListStringFourPurpleReturnListColorFourPurple() {
+        this.humanColorBuilder = new HumanColorBuilder();
+        this.expectedColor = new ArrayList<>();
+        this.resultColor = new ArrayList<>();
+        resultColor = humanColorBuilder.buildColor(new String[]{"Purple","Purple","Purple","Purple"});
+        expectedColor.add(Color.Purple);
+        expectedColor.add(Color.Purple);
+        expectedColor.add(Color.Purple);
+        expectedColor.add(Color.Purple);
+        assertThat(resultColor, equalTo(expectedColor));
+    }
 }
