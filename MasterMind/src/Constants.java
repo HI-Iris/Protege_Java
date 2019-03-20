926151d1a63bd2eda13104c8d237c1d9a230cdf5
iris.hou <xinzhu.hou@gmail.com>
2019-03-21 08:55:53 +1100

    Group the constants by space

--- MasterMind/src/Constants.java
+++ MasterMind/src/Constants.java
@@ -2,11 +2,14 @@ class Constants {
     static final String ERROR_INVALID_COLOR = "Error: you have given an invalid colour!";
     static final String ERROR_INVALID_LENGTH = "Error: you must pass 4 colours!";
     static final String ERROR_TRY_OVER_60 = "Error: you have had more than 60 tries!";
+
     static final String MSG_WIN = "You won!";
     static final String MSG_WELCOME = "Welcome to the Master Mind Game\nThe Master Mind has selected four colors from Red, Blue, Green, Orange, Purple and Yellow!";
-    static final String MSG_ENTER_COLOR = "Please enter 4 colors, separate by ','";
-    static final String MSG_COLOR_MATCH = "Here is the result:";
+    static final String MSG_ENTER_COLOR = "Please select 4 colors from the list above, separate by ','";
+    static final String MSG_COLOR_CHECK = "Here is the result:";
+    static final String MSG_COLOR_NO_MATCH = "The colors you entered doesn't match any.";
+
     static final int NUM_OF_COLOR_IN_GAME = 4;
     static final int NUM_OF_COLOR_IN_TOTAL = 6;
-    static final int MAX_ATTEMPT = 60;
+    static final int MAX_ATTEMPT = 10;
 }
