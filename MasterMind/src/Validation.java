2da6124ca97ddcaf09f395c991990624d72bd18a
iris.hou <xinzhu.hou@gmail.com>
2019-03-21 09:19:09 +1100

    Write two more validation to check the status of game

--- MasterMind/src/Validation.java
+++ MasterMind/src/Validation.java
@@ -1,9 +1,25 @@
+import java.util.List;
+
 class Validation {
-    static boolean isColor(String userInput) {
-        for (Color color : Color.values())
-            if (userInput.toUpperCase().equals(color.name().toUpperCase())) {
-                return true;
-            }
-        return false;
+
+    static boolean isFourColorList(List<Color> colorList) {
+        boolean flag = colorList.size() == Constants.NUM_OF_COLOR_IN_GAME;
+        if (colorList.size() > Constants.NUM_OF_COLOR_IN_GAME) {
+            new ParserConsole().printMSG(Constants.ERROR_INVALID_LENGTH);
+        }
+        if (colorList.size() < Constants.NUM_OF_COLOR_IN_GAME) {
+            new ParserConsole().printMSG(Constants.ERROR_INVALID_COLOR);
+        }
+        return flag;
+    }
+
+    static boolean areResultColorsAllBlack(List<Result> resultColors) {
+        return !resultColors.isEmpty() && resultColors.stream().distinct().limit(2).count() <= 1 && resultColors.get(0) != Result.White;
+    }
+
+    static void isAttemptsOver60(int attempts){
+        if(attempts<Constants.MAX_ATTEMPT){
+            new ParserConsole().printMSG(Constants.MSG_WIN);
+        }else new ParserConsole().printMSG(Constants.ERROR_TRY_OVER_60);
     }
 }
