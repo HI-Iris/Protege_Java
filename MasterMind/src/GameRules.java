f0585fb09749d98ad05ce014d41785fb2a7c58c1
iris.hou <xinzhu.hou@gmail.com>
2019-03-21 09:24:41 +1100

    Rename the method, pass two List<Color> as parameter to compare

--- MasterMind/src/GameRules.java
+++ MasterMind/src/GameRules.java
@@ -2,14 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 class GameRules {
-    static List<Result> match(List<Color> humanColor, MasterMind masterMind) {
-        List<Color> masterColors = masterMind.getMasterColors();
+    List<Result> check(List<Color> humanColors, List<Color> masterColors) {
         List<Result> result = new ArrayList<>();
-        for (int i = 0; i < humanColor.size(); i++) {
-            if (humanColor.get(i) == masterColors.get(i)) {
+        for (int i = 0; i < humanColors.size(); i++) {
+            if (humanColors.get(i) == masterColors.get(i)) {
                 result.add(Result.Black);
             }
-            if ((masterColors.contains(humanColor.get(i))) && (humanColor.get(i) != masterColors.get(i))) {
+            if ((masterColors.contains(humanColors.get(i))) && (humanColors.get(i) != masterColors.get(i))) {
                 result.add(Result.White);
             }
         }
