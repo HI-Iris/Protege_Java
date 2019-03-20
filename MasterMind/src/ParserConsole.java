0ef05e0db1330f15a88167de0a817d98ff693f0b
iris.hou <xinzhu.hou@gmail.com>
2019-03-21 08:59:33 +1100

    Add interface and its implementation to deal with the input and output

---
+++ MasterMind/src/ParserConsole.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class ParserConsole implements Parser {
+    private ColorBuilder humanColorBuilder = new HumanColorBuilder();
+
+    @Override
+    public List<Color> getValidInput() {
+        String userInput;
+        List<Color> humanColor;
+        do {
+            userInput = new Scanner(System.in).nextLine();
+            humanColor = humanColorBuilder.buildColor(userInput.split(","));
+        } while (!Validation.isFourColorList(humanColor));
+        return humanColor;
+    }
+
+    @Override
+    public void printMSG(String msg) {
+        System.out.println(msg);
+    }
+
+    @Override
+    public void printList(List list) {
+        for (Result result : (Iterable<Result>) list) {
+            System.out.print(result + " ");
+        }
+        if (list.size() == 0) {
+            printMSG(Constants.MSG_COLOR_NO_MATCH);
+        } else {
+            printMSG("");
+        }
+    }
+}
