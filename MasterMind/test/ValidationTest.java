fd70ad4c5ce4b7491c646f0f5f1204288eb6e4c3
iris.hou <xinzhu.hou@gmail.com>
2019-03-21 08:57:53 +1100

    Refactor the tests, replace array with list

--- MasterMind/test/ValidationTest.java
+++ MasterMind/test/ValidationTest.java
@@ -1,19 +1,60 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 
 public class ValidationTest {
+    private List<Color> colorList;
+    private List<Result> resultColors;
+
     @Test
-    public void testInputIsColor() {
-        String colorRed = "Red";
-        assertTrue(Validation.isColor(colorRed));
+    public void givenFourColorListReturnTrue() {
+        this.colorList = new ArrayList<>();
+        colorList.add(Color.Red);
+        colorList.add(Color.Blue);
+        colorList.add(Color.Yellow);
+        colorList.add(Color.Purple);
+        assertTrue(Validation.isFourColorList(colorList));
     }
 
     @Test
-    public void testInputIsNotColor() {
-        String notColor = "This is not a color";
-        assertFalse(Validation.isColor(notColor));
+    public void givenTwoColorListReturnFalse() {
+        this.colorList = new ArrayList<>();
+        colorList.add(Color.Red);
+        colorList.add(Color.Blue);
+        assertFalse(Validation.isFourColorList(colorList));
     }
 
+    @Test
+    public void givenFourWhiteReturnFalse(){
+        this.resultColors = new ArrayList<>();
+        resultColors.add(Result.White);
+        resultColors.add(Result.White);
+        resultColors.add(Result.White);
+        resultColors.add(Result.White);
+        assertFalse(Validation.areResultColorsAllBlack(resultColors));
+    }
+
+    @Test
+    public void givenFourBlackReturnTrue(){
+        this.resultColors = new ArrayList<>();
+        resultColors.add(Result.Black);
+        resultColors.add(Result.Black);
+        resultColors.add(Result.Black);
+        resultColors.add(Result.Black);
+        assertTrue(Validation.areResultColorsAllBlack(resultColors));
+    }
+
+    @Test
+    public void givenTwoBlackTwoWhiteReturnFalse(){
+        this.resultColors = new ArrayList<>();
+        resultColors.add(Result.Black);
+        resultColors.add(Result.Black);
+        resultColors.add(Result.White);
+        resultColors.add(Result.White);
+        assertFalse(Validation.areResultColorsAllBlack(resultColors));
+    }
 }
